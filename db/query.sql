26ad498c5c3e85ca0af506bbd6cf63413f908dcc
Dinimar Fish <tayla@tallisman.com.au>
2021-08-28 19:02:51 +1000

    added multiple tables to initial query

--- db/query.sql
+++ db/query.sql
@@ -1,9 +1,10 @@
 SELECT 
     employee.employee_id AS id, 
     employee.first_name, employee.last_name,
-    staff_role.role_id, staff_role.title
-FROM staff_role
-JOIN employee
-ON employee.role_id = staff_role.role_id
+    staff_role.title, department.dept_name AS department
+FROM employee
+INNER JOIN staff_role
+ON employee.role_id = staff_role.role_id 
+INNER JOIN department
+ON staff_role.department_id = department.id
 ORDER BY id ASC;
-
